b2dea8b8df09ba3c12e8ef605c71bcf08191443e
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2025-07-15 11:49:48 -0500

    notes

---
+++ src/generic-extract.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+# Generic ROI extraction
+
+# First target: ALFFs for NegVal
+
+# Inputs:
+#   roi filename from ../rois dir (generate roi label filename from this)
+#   target image to extract data from (or list of target images and store tags from their filenames)
+#   background image for registration overlay
+#   show overlay/rois on standard image yes/no
+#   mask to show on standard image (if given)
+
+# Follow nilearn procedures in extract-rois-gfeat.py
