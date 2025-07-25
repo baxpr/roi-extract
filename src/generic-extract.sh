cd63ca9e4d50dc16462df5555acb266b675aed8e
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2025-07-25 12:24:25 -0500

    generic-wip

--- src/generic-extract.sh
+++ src/generic-extract.sh
@@ -12,3 +12,33 @@
 #   mask to show on standard image (if given)
 
 # Follow nilearn procedures in extract-rois-gfeat.py
+
+
+# Inputs
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in      
+        --roi_niigz)        export roi_niigz="$2";        shift; shift ;;
+        --out_dir)          export out_dir="$2";          shift; shift ;;
+        --underlay_niigz)   export underlay_niigz="$2";   shift; shift ;;
+        --mask_niigz)       export mask_niigz="$2";       shift; shift ;;
+        --show_std)         export show_std="$2";         shift; shift ;;
+        --tgts_niigz)
+            next="$2"
+            while ! [[ "$next" =~ -.* ]] && [[ $# > 1 ]]; do
+                tgts_niigz+=("$next")
+                shift
+                next="$2"
+            done
+            shift ;;
+        *) echo "Input ${1} not recognized"; shift ;;
+    esac
+done
+
+
+# Find ROI image and labels
+roi_dir=$(dirname "${BASH_SOURCE[0]}")/../rois
+export roi_tsv=$(echo "${roi_niigz}" | cut -d'_' -f1)
+roi_tsv="${roi_dir}"/"${roi_tsv}_dseg.tsv"
+roi_niigz="${roi_dir}"/"${roi_niigz}"
+
